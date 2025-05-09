e78bbf0f67272c930b8c13a25102916583687e16
Walter Vergara <walterwrv290888@gmail.com>
2025-05-09 13:13:26 -0300

    Clean código

--- src/components/CreatePerfil.jsx
+++ src/components/CreatePerfil.jsx
@@ -56,39 +56,43 @@ const CreatePerfil = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={`space-y-4 p-4 max-w-md mx-auto ${modoOscuro === "oscuro" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className={`space-y-4 p-4 max-w-md mx-auto ${modoOscuro === "oscuro" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}>
+        
+        <div>
+          <label className="block mb-1 font-semibold">Nombre del perfil</label>
+          <input
+            type="text"
+            {...register('name', { required: 'El nombre de perfil es obligatorio' })}
+            className="w-full p-2 border rounded"
+          />
+          {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+        </div>
+
+        <div>
+          <label className="block mb-1 font-semibold">Categoría de edad</label>
+          <select
+            {...register('ageCategory', { required: 'La categoría por edad es obligatoria' })}
+            className="w-full p-2 border rounded"
+          >
+            <option value="Infantil">Infantil</option>
+            <option value="Adulto">Adulto</option>
+          </select>
+          {errors.ageCategory && <p className="text-red-500 text-sm">{errors.ageCategory.message}</p>}
+        </div>
+
+        <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">
+          Crear perfil
+        </button>
+      </form>
       <button
+        type="button"
         onClick={() => navigate('/administrar-perfiles')}
         className="fixed bottom-6 right-6 px-4 py-2 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 transition"
       >
         Volver
       </button>
-      <div>
-        <label className="block mb-1 font-semibold">Nombre del perfil</label>
-        <input
-          type="text"
-          {...register('name', { required: 'El nombre de perfil es obligatorio' })}
-          className="w-full p-2 border rounded"
-        />
-        {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
-      </div>
-
-      <div>
-        <label className="block mb-1 font-semibold">Categoría de edad</label>
-        <select
-          {...register('ageCategory', { required: 'La categoría por edad es obligatoria' })}
-          className="w-full p-2 border rounded"
-        >
-          <option value="Infantil">Infantil</option>
-          <option value="Adulto">Adulto</option>
-        </select>
-        {errors.ageCategory && <p className="text-red-500 text-sm">{errors.ageCategory.message}</p>}
-      </div>
-
-      <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">
-        Crear perfil
-      </button>
-    </form>
+  </>
   );
 };
 
